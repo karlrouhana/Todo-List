29143aee648d59b3fe0a286629f992ed3ad0db3b
karlrouhana <karlrouhana58@gmail.com>
2024-06-10 16:43:36 +0300

    CSS: Added Crossed To Tasks

--- src/Components/task/Task.js
+++ src/Components/task/Task.js
@@ -1,16 +1,24 @@
 // Task.js
-import React from 'react';
+import React, { useState } from 'react';
 import './task.css';
 
 const Task = ({ task, deleteTask }) => {
+  const [checked, setChecked] = useState(false);
+
+  function handleCheck(){
+    setChecked(!checked);
+  }
+
   return (
     <div className="task">
       <div class="task__content">
-        <input type="checkbox" />
-        <p>{task.name}</p>
-        <p>{task.date}</p>
+        <input type="checkbox" onClick={handleCheck}/>
+        <div class="task__content-details">
+          <p className={checked ? 'crossed' : ''}>{task.name}</p>
+          <p className={checked ? 'crossed' : ''}>{task.date}</p>
+        </div>
       </div>
-      <button onClick={() => deleteTask(task)}>Delete</button>
+      <button onClick={() => deleteTask(task)} className="delete-btn">Delete</button>
     </div>
   );
 }
